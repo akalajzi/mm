5ac130a75526b7a2efab931b68fe4620f263e909
akalajzi <akalajzi@gmail.com>
2017-05-06 02:47:30 +0200

    create zones

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="zoneGroup-side">
+          <div className="zone topLeft">top left</div>
+          <div className="zone bottomLeft">bottom left</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="zoneGroup-center">
+          <div className="zone topCenter">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="zone center">center</div>
+          <div className="zone bottomCenter">bottom center</div>
+        </div>
+        <div className="zoneGroup-side">
+          <div className="zone topRight">top right</div>
+          <div className="zone bottomRight">bottom right</div>
+        </div>
       </div>
     );
   }
